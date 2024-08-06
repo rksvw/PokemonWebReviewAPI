816c93931642a225be5176a1c9f2e01d3918218d
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-08-06 09:30:23 +0530

    Add project files.

---
+++ GameStore/modals/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.modals
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string iZecht { get; set; }
+    }
+}
