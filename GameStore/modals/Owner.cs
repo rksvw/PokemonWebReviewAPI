8902ad8eaed39b43a3e9f994c881aa200f51dd50
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-08-06 10:01:07 +0530

    Many-To-Many relationship in modals

--- GameStore/modals/Owner.cs
+++ GameStore/modals/Owner.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string IZecht { get; set; }
         public Country Country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
 
     }
 }
